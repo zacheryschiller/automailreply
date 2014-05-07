276a448b418f7b6cd5064d588e5566c75443a1d8
Zachery <zacheryschiller@gmail.com>
2014-05-07 09:49:21 -0400

    It's working! ...partially. Read in 26000 messages, built the dictionary and actually made a match based on a message. Need to save corpus and dictionary so they aren't created every time.

--- main.py
+++ main.py
@@ -25,12 +25,19 @@ class Message:
     def __init__(self, fileLocation):
         Message.messageCount += 1
         self.fileLocation = fileLocation
-        self.plainText = readEmail(self.fileLocation)
+        print "creating",str(Message.messageCount)
+        self.plainText,self.sender,self.subject = readEmail(self.fileLocation)
         self.keywords = formatEmail(self.plainText)
         addToArchive(self.keywords)
 
     def getCount(self): 
         return Message.messageCount
+
+    def getSender(self):
+        return self.sender
+    
+    def getSubject(self):
+        return self.subject
 
     def getFileLocation(self): 
         return self.FileLocation
@@ -56,13 +63,15 @@ def readEmail(filename):
     #print 'From: %s' % message['from']
     #print 'Subject: %s' % message['subject']
     parts = message.get_payload()
-    #TODO Check for message before getting payload
-    #print parts
+    if (isinstance(parts, basestring)):
+        return parts,message['from'],message['subject']
     msg = parts[0].get_payload()
-    msg = msg + ' ' + message['from']
-    msg = msg + ' ' + message['subject']
-    #print parts[1].get_payload()
-    return msg
+    while not(isinstance(msg, basestring)):
+        msg = msg[0].get_payload()
+    #print "-------------------------------------------------------"
+    #print msg
+    return msg,message['from'],message['subject']
+
 
 ## Format the txt from an email to be used in the comparison
 def formatEmail(txt):
@@ -101,21 +110,25 @@ def checkKeywords(msg):
 def splitEmails(filename, locToSave):
     fin = open(filename, 'r')
     data = fin.readlines()
-    count = 0
-    f = False
     listEmails = []
+
+    start = True
     for line in data:
-        if f == False:
+        if start == True:
+            name = locToSave+'email0.txt'
+            listEmails.append(name)
+            fout = open(name, 'w')
+            count = 0
+            start = False
+        elif (line[:5] == "From "):
+            count = count+1
+            fout.close()
             name = locToSave+'email'+str(count)+'.txt'
             listEmails.append(name)
             fout = open(name, 'w')
-            f = True
         fout.write(line)
-        #TODO Keep first file open while writting first msg
-        if (line[:5] == "From "):
-            count = count+1
-            fout.close()
-            f = False
+
+    fout.close()
     fin.close()
     print "Created " + str(count) + " files from emails."
     return listEmails
@@ -139,19 +152,15 @@ def loadKeywords():
 ## Takes in newKeyword and returns T/F if added
 ## Checks to ensure not adding a duplicate
 def addKeyword(newKeyword):
-    # TODO Fix with updated global keywordList
-    fin = open("keywords.txt",'r+')
-    data = fin.readlines()
-    #print newKeyword in keywordList
-    for i in range(len(data)):
-	line = data[i]
-	line = line.split(',')
-	if (line[0] == newKeyword):
-	    fin.close()
-	    return False
-    s = str(newKeyword)+",0\n"
-    fin.write(s)
-    fin.close()
+    global keywordList
+    for i in range(len(keywordList)):
+        if newKeyword == keywordList[i][0]:
+            return False
+    fout = open("keywords.txt",'w')
+    for i in range(len(keywordList)):
+	l = str(keywordList[i][0])+','+str(keywordList[i][1])+'\n'
+        fout.write(l)
+    fout.close()
     return True
 
 ## Remove keyword from keyword file
@@ -202,7 +211,7 @@ def getInfo():
     while (mbox == 'i'):
         mbox = raw_input("Do you have a .mbox archive of your email to use? i=more info (y/n/i): ")
         if mbox == 'y':
-            fileLoc = raw_input(("Please enter the location and name of your .mbox file (ex: /users/you/Downlods/myEmails.mbox): ")
+            fileLoc = raw_input(("Please enter the location and name of your .mbox file (ex: /users/you/Downlods/myEmails.mbox): "))
             locToSave =  raw_input("Please enter the location that you would like to save these emails (ex: /users/you/Documents/myEmails/): ")
             cont = raw_input("Are you sure you want to use your .mbox? This will split the archive into individual email files. Careful as this does make a lot of files... (y/n): ")
             if cont == 'y':
@@ -226,8 +235,8 @@ def getInfo():
 
 ## Main function that runs
 def main():
-    #allMail = getInfo()
-    allMail = ["testEmail1.txt", "testEmail2.txt", "testEmail3.txt", "testEmail4.txt"]
+    allMail = getInfo()
+    #allMail = ["testEmail1.txt", "testEmail2.txt", "testEmail3.txt", "testEmail4.txt"]
     arrayMessages = []
     for mail in allMail:
         arrayMessages.append(Message(mail))
