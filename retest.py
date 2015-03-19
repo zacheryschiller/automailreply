bbcbc4ae716c51bb6bf8c1f9710543eb933234a3
Zachery Schiller <zschiller@Zack-Schiller-MacBookProRetina-2012.orono.u87.k12.me.us>
2015-03-19 13:27:21 -0400

    Testing after moving workspace.

---
+++ retest.py
@@ -0,0 +1,29 @@
+# !/usr/bin/python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+################################  Information ################################
+##
+## Title: 
+##
+## Author: Zachery Schiller
+## Email: zacheryschiller@gmail.com
+## Github: https://github.com/zacheryschiller/
+## 
+##############################################################################
+
+###### 
+## 
+
+## Imports
+from math import *
+
+## Main program
+def main():
+    print "Ignore this. Just a test."
+
+## Run the main program
+if __name__ == '__main__':
+    main()
+
+##
+######
